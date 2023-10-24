ac675bc0e64fa462c700dcc117e826e76f681458
root <root@DESKTOP-UV0CABD>
2023-10-24 20:43:53 +0200

    Java completed

---
+++ src/Arithmetic.java
@@ -0,0 +1,15 @@
+public class Arithmetic {
+
+    public int sum(int a, int b) {
+    return a + b;
+    }
+
+    public int difference(int a, int b) {
+        return a - b;
+    }
+
+
+    public double averege(double a, double b) {
+        return (a + b)/2;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        Arithmetic sum_calculation = new Arithmetic();
+        Arithmetic difference_calculation = new Arithmetic();
+        Arithmetic averege_calculation = new Arithmetic();
+
+        System.out.println(sum_calculation.sum(5, 3));
+        System.out.println(difference_calculation.difference(2, 5));
+        System.out.println(averege_calculation.averege(3.0, 4.0));
+
+    }
+}
