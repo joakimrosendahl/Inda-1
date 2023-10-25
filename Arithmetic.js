ab3fed6387533b92c96b0e4567aa470f8fcb775e
root <root@DESKTOP-UV0CABD>
2023-10-25 12:55:15 +0200

    completed labb in js

---
+++ Arithmetic.js
@@ -0,0 +1,18 @@
+function sum(a, b) {
+    return a + b;
+} 
+
+function difference(a, b) {
+    return a - b;
+}
+
+function avrege(a, b) {
+    return (a + b)/2;
+}
+    
+console.log(sum(5, 3));
+console.log(difference(2, 5));
+console.log(avrege(3.0, 4.0));
+
+
+
