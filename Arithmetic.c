7ffe748d81f5b62613144380b35cdf50433bf923
root <root@DESKTOP-UV0CABD>
2023-10-26 13:15:54 +0200

    Finished labb in C

--- Arithmetic.c
+++ Arithmetic.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
-        int sum(int a, int b) {
+    int sum(int a, int b) {
         return a + b;
     }
 
-
-
-    int main(int argc, char *argv[]) {
-        //int a = 3;
-        //int b = 5;
-        int ret;
-
-        ret = sum(3, 5);
-        printf("%d\n", ret);
-       
-
-        return 0;
+    int difference(int a, int b) {
+        return a - b;
     }
 
+    double avrege(double a, double b) {
+        return (a + b)/2;
+    }
 
+    int main(int argc, char *argv[]) {
+
+        printf("%d\n", sum(3, 5));
+        printf("%d\n", difference(2, 5));
+        printf("%f\n", avrege(3.0, 4.0));
+       
+
+        return 0; 
+    }
